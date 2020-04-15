59ad4b8937e75dddecb2f00199a1b6755492e178
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-04-14 23:40:34 -0300

    Add files via upload
    Solución propuesta ejercicio: 
    Escribir un programa que lea un entero positivo, 𝑛, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta 𝑛

---
+++ enteros.py
@@ -0,0 +1,5 @@
+entero = int(input("Ingrese un numero entero positivo: "))
+
+suma = str(int((entero*(entero+1))/2))
+entero = str(entero)
+print("La suma de los enteros positivos desde 1 hasta "+entero+" es: "+suma)
